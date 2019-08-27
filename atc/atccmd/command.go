0f5ea99e77623daade84c09cbe3041df231d32f6
Zoe Tian <ztian@pivotal.io>
2019-08-27 16:56:19 -0400

    [fly,atc,db] team scoped fly containers:
      - Adds `--team-name` flag to `fly containers`
      - Adds `--all-teams` flag to `fly containers`
      - Adds new api endpoint `/api/v1/containers` that returns all the
      containers based on the teams assigned to the user (all deployment
      containers in case of super-admin).
      - refactors atc/db/team.go to use `getContainersByTeamIds` function
      from the container_repository.go
      - Adds tests for various contianer parts (api, fly, db)
    
    Signed-off-by: Zoe Tian <ztian@pivotal.io>
    Co-authored-by: Bin Ju <bju@pivotal.io>
    Co-authored-by: Bishoy Youssef <byoussef@pivotal.io>
    Co-authored-by: Taylor Silva <tsilva@pivotal.io>
    Co-authored-by: Mike Hoskins <mhoskins@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -588,7 +588,7 @@ func (cmd *RunCommand) constructAPIMembers(
 	dbPipelineFactory := db.NewPipelineFactory(dbConn, lockFactory)
 	dbJobFactory := db.NewJobFactory(dbConn, lockFactory)
 	dbResourceFactory := db.NewResourceFactory(dbConn, lockFactory)
-	dbContainerRepository := db.NewContainerRepository(dbConn)
+	dbContainerRepository := db.NewContainerRepository(dbConn, lockFactory)
 	gcContainerDestroyer := gc.NewDestroyer(logger, dbContainerRepository, dbVolumeRepository)
 	dbBuildFactory := db.NewBuildFactory(dbConn, lockFactory, cmd.GC.OneOffBuildGracePeriod)
 	dbCheckFactory := db.NewCheckFactory(dbConn, lockFactory, secretManager, cmd.GlobalResourceCheckTimeout)
@@ -786,7 +786,7 @@ func (cmd *RunCommand) constructBackendMembers(
 
 	dbWorkerLifecycle := db.NewWorkerLifecycle(dbConn)
 	dbResourceCacheLifecycle := db.NewResourceCacheLifecycle(dbConn)
-	dbContainerRepository := db.NewContainerRepository(dbConn)
+	dbContainerRepository := db.NewContainerRepository(dbConn, lockFactory)
 	dbArtifactLifecycle := db.NewArtifactLifecycle(dbConn)
 	dbCheckLifecycle := db.NewCheckLifecycle(dbConn)
 	resourceConfigCheckSessionLifecycle := db.NewResourceConfigCheckSessionLifecycle(dbConn)
